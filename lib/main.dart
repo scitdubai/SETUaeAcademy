8f89834d0dd2bd28d1b5d12f57d5a9c9790a65c3
MazenScit <D.mazen@scit.co>
2024-06-29 09:48:05 +0300

    five commit

--- lib/main.dart
+++ lib/main.dart
@@ -10,9 +10,16 @@ import 'logale/logale.dart';
 
 
 void main() async {
+  HttpOverrides.global = MyHttpOverrides();
   runApp(const MyApp());
 }
-
+class MyHttpOverrides extends HttpOverrides{
+  @override
+  HttpClient createHttpClient(SecurityContext? context){
+    return super.createHttpClient(context)
+      ..badCertificateCallback = (X509Certificate cert, String host, int port)=> true;
+  }
+}
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
   @override
